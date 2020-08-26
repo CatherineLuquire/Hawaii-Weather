aeb12022fef8d5b274e7f407144669f69cdc29bc
CatherineLuquire <cluquire@gmail.com>
2020-08-25 18:46:05 -0700

    added commits to app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def precipitation():
     results = session.query(Measurement.date, Measurement.prcp).all()
     session.close()
 
-    # Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
+    # Convert the query results to a dictionary using `date` as the key and `prcp` as the value, and add to list.
 
     precip_data = []
     for date, precipitation in results:
@@ -57,51 +57,64 @@ def precipitation():
         precip_dict['precipitation'] = precipitation
         precip_data.append(precip_dict)
 
-    # Return the JSON representation of your dictionary.
+    # Return the JSON representation of dictionary.
     return jsonify(precip_data)
 
 
 @app.route('/api/v1.0/stations')
 def stations():
+    # create session from python to db
     session = Session(engine)
+    # query station names
     results = session.query(Station.name).all()
     session.close()
-
+    # convert tuple into normal list
     all_stations = list(np.ravel(results))
+
     return jsonify(all_stations)
 
 @app.route('/api/v1.0/tobs')
 def tobs():
+    # create session from python to db
     session = Session(engine)
+    # query measurement dates and temps for a year from station USC00519281
     results = session.query(Measurement.date, Measurement.tobs).\
     filter(Measurement.date >= '2016-08-23').\
     filter(Measurement.date <= '2017-08-23').\
     filter(Measurement.station == "USC00519281").all()
     session.close()
-
+    # convert query results into a dictionary with date as the key and temp as value, and add to list
     temp_data = []
     for date, temperature in results:
         temp_dict = {}
         temp_dict['date'] = date
         temp_dict['temperature'] = temperature
         temp_data.append(temp_dict)
-
+    
+    # Return the JSON representation of dictionary.
     return jsonify(temp_data)
 
 @app.route('/api/v1.0/temp/<start>')
+# create decorator with no end date
 def temp(start=None):
+    # create session from python to db
     session = Session(engine)
+    # query results
     results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs).\
        filter(Measurement.date >= start)).all()
-    
+       
+    # Return the JSON representation of dictionary.
     return jsonify(results)
 
 @app.route('/api/v1.0/temp/<start>/<end>')
+# create second decorator with start and end date
 def temp_end(start=None,end=None):
+    # create session from python to db
     session = Session(engine)
+    # query results
     results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs).\
        filter(Measurement.date >= start).filter(Measurement.date <= end)).all()
-    
+    # return json representation of dictonary
     return jsonify(results)
 
 
